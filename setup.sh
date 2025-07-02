582895b9fc17a80290d7efb4836a13c73b2b3439
Nassim <113632528+sspiider@users.noreply.github.com>
2025-07-02 01:35:20 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Force Python 3.9 even if Cloud tries 3.13
-conda create -n myenv python=3.9 -y
-source activate myenv
+conda create -n py39 python=3.9 -y
+source activate py39
 pip install -r requirements.txt
